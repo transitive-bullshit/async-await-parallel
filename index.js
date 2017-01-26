799590c7d094ebf44910dd67db60ba51769b023c
Travis Fischer <fisch0920@gmail.com>
2017-01-26 13:32:15 -0800

    adding jsdoc comments to source

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 module.exports = parallelMap
 module.exports.pool = parallelPool
 
+/**
+ * Invokes an array of async functions in parallel with a limit to the maximum
+ * number of concurrent invocations. Async functions are executed in-order and
+ * the results are mapped to the return array.
+ *
+ * Acts as a replacement for `await Promise.all([ ... ])` by limiting the max
+ * concurrency of the array of function invocations.
+ *
+ * If any single task fails (eg, returns a rejected Promise), the pool will drain
+ * any remaining active tasks and reject the resulting Promsie.
+ *
+ * @param {Array<async Function(Void) => Any>} thunks
+ * @param {Number?} concurrency Max concurrency (defaults to 5)
+ *
+ * @return {Promise<Array<Any>>}
+ */
 async function parallelMap (thunks, concurrency = 5) {
   if (concurrency > 0) {
     concurrency = Math.min(concurrency, thunks.length)
@@ -24,6 +40,21 @@ async function parallelMap (thunks, concurrency = 5) {
   return results
 }
 
+/**
+ * Executes a given async `task` multiple times in parallel with a guaranteed
+ * max concurrency given by `size`.
+ *
+ * The task should be an async function which resolves to a boolean for whether
+ * or not there are more tasks to process.
+ *
+ * If any single task fails (eg, returns a rejected Promise), the pool will drain
+ * any remaining active tasks and reject the resulting Promsie.
+ *
+ * @param {Number} size
+ * @param {async Function(Void) => Boolean} task
+ *
+ * @return {Promise<Void>}
+ */
 async function parallelPool (size, task) {
   let done = false
   let active = 0
@@ -37,10 +68,12 @@ async function parallelPool (size, task) {
         task()
           .then((more) => {
             if (--active <= 0 && (done || !more)) {
-              if (!errors.length) {
-                return resolve()
-              } else {
+              if (errors.length > 0) {
+                // at least one task failed
                 return reject(new Error(errors))
+              } else {
+                // all tasks completed successfully
+                return resolve()
               }
             } else if (more) {
               poolIterator()
@@ -51,8 +84,13 @@ async function parallelPool (size, task) {
           .catch((err) => {
             errors.push(err)
             done = true
+
             if (--active <= 0) {
               return reject(new Error(errors))
+            } else {
+              // wait until all active tasks are drained before rejecting the
+              // final result (no new tasks will be started now that we're in
+              // this error state)
             }
           })
       }
